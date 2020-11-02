3e72340355e6b46a854962ad83da32db4ecf1233
黄孝哲 <huangxiaozhe@lakala.com>
2020-11-02 17:08:05 +0800

    更新demo

--- src/Arr2Table.php
+++ src/Arr2Table.php
@@ -11,7 +11,7 @@ class Arr2Table
     public static $header = '<!doctype html> <html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <title>可视化数据</title> <style type="text/css"> .customers { font-family: "Trebuchet MS", Arial, Helvetica, sans-serif; width: 100%; border-collapse: collapse; } .customers td, .customers th { font-size: 1em; border: 1px solid #1E9FFF; text-align: left; } .customers th { font-size: 1.1em; text-align: left; background-color: #1E9FFF; color: #ffffff; } .customers tr.autocolor td { color: #000000; background-color: #EAF2D3; } </style> </head><body>';
     public static $footer = '</body></html>';
     //获取表标题
-    public static function moreTable($data = [], $isMore = false)
+    public static function moreTable($data = [])
     {
         if (empty($data)) return '无数据';
         $table ='';
--- test/test.php
+++ test/test.php
@@ -1,13 +1,36 @@
 <?php
 require_once '../vendor/autoload.php';
 $data = [
-        ['类型','GMV','收入'],
-        [1,2,3],
-        [1,2,3],
-        [1,2,3]
+    ['单个表格'],
+    ['标题1', '标题2', '标题3'],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3]
 ];
+
 use Hxzlhby\Arr2Table;
 
-$table = Arr2Table::getHtmlTable($data, 'test');
+$table = Arr2Table::oneTable($data);
+
+echo $table;
+
+$data2 = [
+    [
+        ['多个表格1'],
+        ['标题1', '标题2', '标题3'],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ],
+    [
+        ['多个表格2'],
+        ['标题1', '标题2', '标题3'],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ]
+];
+
+$table2 = Arr2Table::moreTable($data2);
 
 echo $table;
