209f90c28b8229e610e2020fe6addeb4b0524b9f
黄孝哲 <huangxiaozhe@lakala.com>
2020-11-03 11:09:57 +0800

    注释

--- src/Arr2Table.php
+++ src/Arr2Table.php
@@ -1,16 +1,20 @@
 <?php
 /**
-* 数组转换为html的table
-* @date: 2018年5月2日 下午2:18:32
-* @author: hxz
+ * 描述：数组转换为表格
+ * 专为 email 设计
 */
 namespace Hxzlhby;
 
 class Arr2Table
 {
-    public static $header = '<!doctype html> <html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <title>可视化数据</title> <style type="text/css"> .customers { font-family: "Trebuchet MS", Arial, Helvetica, sans-serif; width: 100%; border-collapse: collapse; } .customers td, .customers th { font-size: 1em; border: 1px solid #1E9FFF; text-align: left; } .customers th { font-size: 1.1em; text-align: left; background-color: #1E9FFF; color: #ffffff; } .customers tr.autocolor td { color: #000000; background-color: #EAF2D3; } </style> </head><body>';
-    public static $footer = '</body></html>';
-    //获取表标题
+    private static $header = '<!doctype html> <html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <title>可视化数据</title> <style type="text/css"> .customers { font-family: "Trebuchet MS", Arial, Helvetica, sans-serif; width: 100%; border-collapse: collapse; } .customers td, .customers th { font-size: 1em; border: 1px solid #1E9FFF; text-align: left; } .customers th { font-size: 1.1em; text-align: left; background-color: #1E9FFF; color: #ffffff; } .customers tr.autocolor td { color: #000000; background-color: #EAF2D3; } </style> </head><body>';
+    private static $footer = '</body></html>';
+
+    /**
+     * 多表格
+     * @param array $data
+     * @return string
+     */
     public static function moreTable($data = [])
     {
         if (empty($data)) return '无数据';
@@ -21,7 +25,12 @@ class Arr2Table
         return self::$header.$table.self::$footer;
     }
 
-    public static function oneTable($data)
+    /**
+     * 单表格
+     * @param array $data
+     * @return string
+     */
+    public static function oneTable($data = [])
     {
         if (empty($data)) return '无数据';
         $table = self::oneTable($data);
